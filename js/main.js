ce951314c449cde32378efc1e465663a13be3dc0
EzexnDev <gianmarco.cavalieredev@gmail.com>
2020-11-18 19:08:57 +0100

    Esercizio completo con bonus

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@ const sliderApp = new Vue({
     data: {
         inputUser: '',
         toDostuff: [],
+        defaultColor: '#F5F7F7'
     },
     methods: {
         addToDo: function() {
-            this.toDostuff.push(this.inputUser);
+            this.toDostuff.push({
+                text: this.inputUser,
+                color: this.defaultColor
+            });
             console.log(this.toDostuff);
             this.inputUser = '';
         },
 
         removeToDo: function(delIndex) {
             this.toDostuff.splice(delIndex, 1);
+        },
+
+        changeBackgroundColor: function(delIndex) {
+            console.log("entro")
+            if (this.toDostuff[delIndex].color == this.defaultColor) {
+                this.toDostuff[delIndex].color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            } else {
+                this.toDostuff[delIndex].color = this.defaultColor;
+            }
+            console.log(this.toDostuff[delIndex].color);
         }
+
     }
 })
